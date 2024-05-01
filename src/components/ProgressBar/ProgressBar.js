b1a2098c63d7058b1ee06f137a7d953fd329a2f2
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-05-01 04:22:28 +0300

    Implement ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,58 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let barHeight = 8;
+  let style = undefined;
+
+  if (size === "medium") {
+    barHeight = 12;
+  } else if (size === "large") {
+    barHeight = 16;
+    style = {
+      "--border-radius": "8px",
+      "--inner-border-radius": "4px",
+      "--padding": "4px",
+    };
+  }
+
+  return (
+    <Wrapper role="progressbar" aria-valuenow={value} style={style}>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarContainer
+        preserveAspectRatio="none"
+        width="100%"
+        height={barHeight}
+        viewBox={`0 0 100 ${barHeight}`}
+      >
+        <Bar x={0} y={0} width={value} height={barHeight} fill="currentColor" />
+      </BarContainer>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  --border-radius: 4px;
+  --inner-border-radius: 0;
+  --padding: 0;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--border-radius);
+  padding: var(--padding);
+  overflow: hidden;
+`;
+
+const BarContainer = styled.svg`
+  border-radius: var(--inner-border-radius);
+  display: block;
+`;
+
+const Bar = styled.rect`
+  color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
