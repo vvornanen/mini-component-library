e5fc74b8d2e383dd50d38227fc9ec21ce3549b94
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-05-01 06:34:41 +0300

    Implement Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,61 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ id, label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <span aria-hidden={true}>{displayedValue}</span>
+      <SelectIcon
+        id="chevron-down"
+        size={24}
+        strokeWidth={2}
+        aria-hidden={true}
+      />
+      <SelectInput id={id} aria-label={label} value={value} onChange={onChange}>
+        {children}
+      </SelectInput>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 18px;
+  color: ${COLORS.gray700};
+  width: fit-content;
+  padding-block: 12px;
+  padding-inline: 16px;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus-within {
+    outline-color: Highlight;
+    outline-color: -webkit-focus-ring-color;
+    outline-style: auto;
+  }
+`;
+
+const SelectInput = styled.select`
+  position: absolute;
+  appearance: none;
+  inset: 0;
+  opacity: 0;
+`;
+
+const SelectIcon = styled(Icon)`
+  margin-inline-end: -6px;
+`;
 
 export default Select;
